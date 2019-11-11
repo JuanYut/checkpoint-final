8f8640e5762f68bf963593017978e8505f33709f
JuanYut <juanyut97@hotmail.com>
2019-11-11 10:39:38 -0600

    Loading element with lazy and suspense :)

--- src/routes/public/Public.routes.jsx
+++ src/routes/public/Public.routes.jsx
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 // * Switch and Route
 import { Switch, Route } from 'react-router-dom';
+// * Ant-Design
+import { Icon } from 'antd';
 
-// * Components
-import HomeContainer from '../../containers/Home/views/Home.container';
-import Error404 from '../../shared/components/Error404/Error404.component';
+function PublicRoutes() {
+  // * Components with lazy
+  const HomeContainer = lazy(() =>
+    import('../../containers/Home/views/Home.container')
+  );
+  const Error404 = lazy(() =>
+    import('../../shared/components/Error404/Error404.component')
+  );
 
-const PublicRoutes = () => {
+  // * Loading Element
+  const Loading = (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <h1>Loading...</h1>
+      <Icon type='loading' style={{ fontSize: '100px' }} />
+    </div>
+  );
+
   return (
     // * Return All the Public Components
-    <Switch>
-      <Route exact path='/' component={HomeContainer} />
-      <Route component={Error404} />
-    </Switch>
+    <Suspense fallback={Loading}>
+      <Switch>
+        <Route exact path='/' component={HomeContainer} />
+        <Route component={Error404} />
+      </Switch>
+    </Suspense>
   );
-};
+}
 
 export default PublicRoutes;
