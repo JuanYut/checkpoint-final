9f9fe798efe09706cb474455276f977db5ff0105
JuanYut <juanyut97@hotmail.com>
2019-11-13 14:36:35 -0600

    Added real text in ServicesHome and green login (｡◕‿◕｡)

--- src/containers/Home/views/components/ServicesHome.component.jsx
+++ src/containers/Home/views/components/ServicesHome.component.jsx
@@ -11,53 +11,60 @@ const ServicesHome = () => {
         src={logoHelpTraffic}
         alt='logo-help-traffic'
       />
-      <h2 className='services-home-title'>Nuestros Servicios</h2>
+      <h2 className='services-home-title'>Nuestros Servicios:</h2>
 
       <h2 className='services-home-subtitle'>
         Brindar ayuda al momento es nuestra especialidad
       </h2>
 
       <h3 className='services-home-description'>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad eaque,
-        doloremque, harum voluptas repudiandae rerum accusamus aut nam, atque
-        iusto nemo autem facere unde voluptatem a ratione explicabo! Aliquid,
-        vitae.
+        HelpTraffic te ofrece la posibilidad de acudir a tu ubicación lo antes
+        posible. Los servicios que ofrecemos son Cambio de llanta, Recarga de
+        gasolina, Cuestiones eléctricas y Servicio de grúa. Conoce mas detalles
+        de cada servicio:
       </h3>
 
       <div className='services-home-options'>
         <div className='services-home-option'>
-          <i class='fas fa-dot-circle fa-3x' style={{ color: '#ff850f' }}></i>
-          <h2 className='services-home-option-title'>Cambio de llanta</h2>
+          <i
+            className='fas fa-dot-circle fa-3x'
+            style={{ color: '#ff850f' }}
+          ></i>
+          <h2 className='services-home-option-title'>Cambio de Llanta</h2>
           <h3 className='services-home-option-desc'>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
-            autem! Obcaecati saepe rerum laboriosam amet.
+            Cambiamos tu llanta en caso de dañarse, ya sea que tu proporciones
+            la llanta nueva, o realices la compra de una llanta con nosotros o
+            cualquier otro proveedor que nos indiques.
           </h3>
         </div>
 
         <div className='services-home-option'>
-          <i class='fas fa-gas-pump fa-3x' style={{ color: '#ff850f' }}></i>
-          <h2 className='services-home-option-title'>Gasolina</h2>
+          <i className='fas fa-gas-pump fa-3x' style={{ color: '#ff850f' }}></i>
+          <h2 className='services-home-option-title'>Recarga de Gasolina</h2>
           <h3 className='services-home-option-desc'>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
-            autem! Obcaecati saepe rerum laboriosam amet.
+            Si te quedaste sin gasolina suficiente nosotros podemos rellenar tu
+            tanque con la cantidad que nos indiques.
           </h3>
         </div>
 
         <div className='services-home-option'>
-          <i class='fas fa-bolt fa-3x' style={{ color: '#ff850f' }}></i>
-          <h2 className='services-home-option-title'>Electrico</h2>
+          <i className='fas fa-bolt fa-3x' style={{ color: '#ff850f' }}></i>
+          <h2 className='services-home-option-title'>Cuestión Eléctrica</h2>
           <h3 className='services-home-option-desc'>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
-            autem! Obcaecati saepe rerum laboriosam amet.
+            Si necesitas recargar la bateria o alguna otra cuestión eléctrica
+            que necesite ser revisada nosotros te podemos auxiliar.
           </h3>
         </div>
 
         <div className='services-home-option'>
-          <i class='fas fa-truck-pickup fa-3x' style={{ color: '#ff850f' }}></i>
-          <h2 className='services-home-option-title'>Grua</h2>
+          <i
+            className='fas fa-truck-pickup fa-3x'
+            style={{ color: '#ff850f' }}
+          ></i>
+          <h2 className='services-home-option-title'>Servicio de Grua</h2>
           <h3 className='services-home-option-desc'>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
-            autem! Obcaecati saepe rerum laboriosam amet.
+            ¿Necesitas mover un vehiculo lo antes posible?, nosotros te
+            proporcionaremos un servicio de grúa seguro para ti y tu vehiculo.
           </h3>
         </div>
       </div>
